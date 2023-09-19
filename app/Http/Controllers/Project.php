a8db6c94ca8908e179bb37a41648f184e559091e
janeloraineflores <janeloraineaflores@yahoo.com>
2023-09-19 17:00:31 +0200

    Aggiunto migrations e seeder

---
+++ app/Http/Controllers/Project.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\sR;
+use Illuminate\Http\Request;
+
+class Project extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(sR $sR)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(sR $sR)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, sR $sR)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(sR $sR)
+    {
+        //
+    }
+}
---
+++ database/seeders/ProjectSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use App\Models\Project;
+
+class ProjectSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Project::truncate();
+
+        for ($i = 0; $i < 30; $i++){
+            $title = substr(fake()->sentence(), 0, 255);
+            
+            Project::create([
+                'title' => $title,
+                'slug' => str()->slug($title),
+                'content' => fake()->paragraph(),
+            ]);
+        }
+    }
+}
