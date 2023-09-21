bdd7ba1bccf3c7a0d82afcba6111d7e94eb6ecf2
janeloraineflores <janeloraineaflores@yahoo.com>
2023-09-21 14:26:34 +0200

    Modificato show

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -4,38 +4,60 @@
 
 @section('main-content')
 <div class="container">
+
     <div class="row py-5">
         <div class="col-12 bg-light">
             <h1>
                 {{ $project->title }}
             </h1>
         </div>
-
-
-        <div class="card col-9 bg-light">
-            <div class="card-body">
-                <span class="card-text">
-                    <strong>
-                        Title:
-                    </strong>
-                    {{ $project->title }}
-                </span>
-                <br>
-                <span class="card-text">
-                    <strong>
-                        Slug:
-                    </strong>
-                    {{ $project->slug }}
-                </span>
-                <br>
-                <span class="card-text">
-                    <strong>
-                        Content:
-                    </strong>
-                    {{ $project->content }}
-                </span>
-            </div>
-        </div>
     </div>
+
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Title</th>
+                <th scope="col">Slug</th>
+                <th scope="col">Content</th>
+                <th scope="col">Actions</th>
+                <th scope="col">Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row">
+                    {{ $project->id }}
+                </th>
+                <td>
+                    {{ $project->title }}
+                </td>
+                <td>
+                    {{ $project->slug }}
+                </td>
+                <td>
+                    {{ $project->content }}
+                </td>
+                <td>
+                    <a href="{{ route('admin.projects.edit', ['project' => $project->id]) }}" class="btn btn-danger">
+                        Update
+                    </a>
+                </td>
+                <td>
+                    <form
+                        action="{{ route('admin.projects.destroy', ['project' => $project->id]) }}"
+                        method="POST"
+                        onsubmit="return confirm('Sei sicuro di voler cancellare questo elemento?');">
+                        @csrf
+                        @method('DELETE')
+                        
+                        <button type="submit" class="btn btn-warning">
+                            Delete
+                        </button>
+                    </form>
+                </td>
+            </tr>
+        </tbody>
+      </table>
 </div>
 @endsection
